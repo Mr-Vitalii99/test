88fe94159bad73281e7988ffc5228c2a951247d9
Mr-Vitalii <co4ckowoi.v@gmail.com>
2023-06-03 12:58:26 +0300

    good

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Context } from './components/Context/Context';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter >
       <Context>
         <App />
       </Context>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
